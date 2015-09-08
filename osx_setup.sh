0010ee9de7adcd422af0a2b028d21ded9ce5d256
teloon <telooon@gmail.com>
2015-09-07 17:39:12 -0700

    update

--- osx_setup.sh
+++ osx_setup.sh
@@ -6,3 +6,7 @@ if [ ! -d "$SOURCE_DIR" ]; then
 fi
 cd $SOURCE_DIR
 git clone https://github.com/teloon/dotfiles.git && cd dotfiles && sh bootstrap.sh
+#install all Bundle plugins
+vim +BundleInstall +qall
+# if command-t doesn't work, run this command:
+# cd ~/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make.
