31dfd0e084d66603387c68ee34e55917c0391b39
teloon <telooon@gmail.com>
2015-09-08 00:35:52 -0700

    update

--- osx_setup.sh
+++ osx_setup.sh
@@ -1,4 +1,4 @@
-cd /Users/$USER
+cd $HOME
 #dotfiles
 SOURCE_DIR="source"
 if [ ! -d "$SOURCE_DIR" ]; then
@@ -10,3 +10,10 @@ git clone https://github.com/teloon/dotfiles.git && cd dotfiles && sh bootstrap.
 vim +BundleInstall +qall
 # if command-t doesn't work, run this command:
 # cd ~/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make.
+
+# on-my-zsh
+cd $HOME/$SOURCE_DIR
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# alias
+echo "alias v='vim'" >> $HOME/.zshrc
